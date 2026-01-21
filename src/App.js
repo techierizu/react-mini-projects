c2271a7f9dccc900577a90854e793e1d91a0bf04
Sk Md Rizwan <101038590+techierizu@users.noreply.github.com>
2026-01-21 07:02:47 +0000

    Improved the repository structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Chess from "./Chess/Chess";
 import MatchPair from "./MatchPair/MatchPair";
+import Stopwatch from "./Stopwatch/Stopwatch";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Chess />
+      {/* <Chess /> */}
       {/* <MatchPair /> */}
+      <Stopwatch />
     </div>
   );
 }
